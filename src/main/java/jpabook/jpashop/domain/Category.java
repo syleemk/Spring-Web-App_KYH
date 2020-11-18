2dd13a224c3fcb411d9a6faa40bda9531e8ffdbf
syleemk <syleemk@gmail.com>
2020-11-18 21:49:02 +0900

    [REFACTORING] 엔티티 코드 최적화

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -19,17 +19,23 @@ public class Category {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "category_item", //다대다관계는 joinTable필요
             joinColumns = @JoinColumn(name = "category_id"), //중간테이블에 있는 카테고리 id
             inverseJoinColumns = @JoinColumn(name = "item_id"))//중간테이블에서 아이템쪽으로 들어가는 item_id
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 편의 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
 
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -22,10 +22,12 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    //영속성 전이
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    //영속성 전이
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delievery_id")
     private Delievery delievery;
 
@@ -35,4 +37,19 @@ public class Order {
     private OrderStatus status;
 
 
+    //== 연관관계 편의 메서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.orders.add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelievery(Delievery delievery){
+        this.delievery = delievery;
+        delievery.setOrder(this);
+    }
 }
